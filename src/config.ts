6c08b21254a7f6a0796d9b386441a719a694ab0a
Nikhil Sharma <nikhil@hotcocoasoftware.com>
2020-05-21 13:23:07 +0530

    initial

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export default {
+  // Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+  // Do not include the trailing slash. See the README for more information:
+  SAMPLE_SERVER_BASE_URL: 'http://YOUR-SERVER-URL',
+  // OR, if you have not set up a web server that runs the learning-opentok-php code,
+  // set these values to OpenTok API key, a valid session ID, and a token for the session.
+  // For test purposes, you can obtain these from https://tokbox.com/account.
+  API_KEY: '46750152',
+  SESSION_ID: '2_MX40Njc1MDE1Mn5-MTU5MDA0MjI4MDU0MH5IR3MxTjlsVm9VQjFEQkVkWk5CRkxFb3Z-fg',
+  TOKEN: 'T1==cGFydG5lcl9pZD00Njc1MDE1MiZzaWc9ODdjMDVhZjdkMzEzZTdhZTY1MDgxMWU2OTU0ZjI5NTJjOWZkMGIyYjpzZXNzaW9uX2lkPTJfTVg0ME5qYzFNREUxTW41LU1UVTVNREEwTWpJNE1EVTBNSDVJUjNNeFRqbHNWbTlWUWpGRVFrVmtXazVDUmt4RmIzWi1mZyZjcmVhdGVfdGltZT0xNTkwMDQyMzAwJm5vbmNlPTAuODQ0MTI2MTQ3NDYzMjA3NiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTkwMDQ1ODk5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9'
+};
